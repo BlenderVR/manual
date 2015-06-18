df5a3cc1a39b6ecd1865f9bc99d8cc17ced926fc
David Poirier-Quinot <davipoir@gmail.com>
2015-06-18 13:46:41 +0200

    mod typos in conf.py (from blender to blender-vr and blender-vrmanual to blendervr-manual)

--- manual/conf.py
+++ manual/conf.py
@@ -220,8 +220,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'blender_manual.tex', 'Blender User Manual',
-   'Blender Community', 'manual'),
+  ('index', 'blendervr-manual.tex', 'BlenderVR User Manual',
+   'BlenderVR Team', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -250,7 +250,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'blender-vrmanual', 'BlenderVR Manual Documentation',
+    ('index', 'blendervr-manual', 'BlenderVR Manual Documentation',
      ['BlenderVR Team'], 1)
 ]
 
