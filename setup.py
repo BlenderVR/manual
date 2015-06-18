d8185054d73265b11088bb9ba18f5c522bf56843
Dalai Felinto <dfelinto@gmail.com>
2015-06-18 11:25:50 -0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='blendervr-manual',
+      version='1.0',
+      description='BlenderVR Manual',
+      author='BlendeVR Dev Team',
+      author_email='blendervr@limsi.fr',
+      url='blendervr.limsi.fr',
+      )
+
