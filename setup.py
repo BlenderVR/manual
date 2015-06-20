cc279cb19846b16a00e9970409f43aff3b5f4020
David Poirier-Quinot <davipoir@gmail.com>
2015-06-20 17:30:19 +0200

    added setup.py for virtualenv creation (for building on read-the-docs server)

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='blendervr-manual',
+      version='0.1',
+      description="BlenderVR docs",
+      long_description="",
+      author='BlenderVR Team',
+      author_email='blendervr@limsi.fr',
+      license='Creative Commons Zero',
+      packages=['manual'],
+      zip_safe=False,
+      install_requires=[
+          '']
+      )
